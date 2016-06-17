c037eb5e3d2a270addfe5aa7f0c30bd91bce96ab
eyelean24 <eyelean24@gmail.com>
2016-06-17 12:13:14 -0400

    Added Sound Hadouken

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
+		playhadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
 		$('.hadouken').finish().show()
@@ -18,10 +18,17 @@ $(document).ready(function() {
 			function() {
 				$(this).hide();
 				$(this).css('left', '520px')
-			});
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playhadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
